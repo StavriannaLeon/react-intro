15ab77e69fedc4672c01691c66cd92a5503c904e
Stavrianna <staleoleo@gmail.com>
2025-05-21 20:30:45 +0300

    feat: add padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <>
             <header className="bg-[#782024] fixed w-full">
-                <div className="container mx-auto flex justify-between items-center">
+                <div className="container mx-auto px-4 flex justify-between items-center">
                     <CodingFactoryLogo/>
                     <a className="text-white hover:underline-offset-4" href="/">Home</a>
                 </div>
