2417b708fb84c98f42f6fcbaf78ff4680d8b1009
Stavrianna <staleoleo@gmail.com>
2025-05-27 19:05:30 +0300

    feat: add counter with reducer example

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,8 @@ import Layout from "./components/Layout.tsx";
 // import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
 // import CounterAdvanced from "./components/CounterAdvanced.tsx";
 // import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
-import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
+// import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
+import CounterWithReducer from "./components/CounterWithReducer.tsx";
 
 function App() {
 
@@ -35,7 +36,8 @@ function App() {
             {/*<CounterWithMoreStates/>*/}
             {/*<CounterAdvanced/>*/}
             {/*<CounterWithCustomHook/>*/}
-            <CounterAdvancedWithCustomHook/>
+            {/*<CounterAdvancedWithCustomHook/>*/}
+            <CounterWithReducer/>
         </Layout>
     </>
   )
