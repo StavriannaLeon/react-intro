4d50dc680c5011a46b5821861b04709eafaf9f50
Stavrianna <staleoleo@gmail.com>
2025-06-10 01:15:00 +0300

    feat: add document titles

--- src/pages/NameChangerPage.tsx
+++ src/pages/NameChangerPage.tsx
@@ -1,6 +1,12 @@
 import NameChanger from "../components/NameChanger.tsx";
+import {useEffect} from "react";
 
 const NameChangerPage = () => {
+
+    useEffect(() => {
+        document.title = "Name Changer";
+    }, []);
+
     return (
         <>
             <NameChanger/>
